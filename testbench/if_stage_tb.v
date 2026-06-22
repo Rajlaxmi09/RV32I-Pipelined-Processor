0b1115a094527c8b8fec6553e4b740d7aae9d736
Rajlaxmi09 <rajlaxmishekhawat90@gmail.com>
2026-06-22 19:09:06 +0530

    Added complete 22-instruction program

---
+++ testbench/if_stage_tb.v
@@ -0,0 +1,47 @@
+`timescale 1ns/1ps
+
+module if_stage_tb;
+
+reg clk;
+reg reset;
+reg stall;
+
+wire [31:0] pc_out;
+wire [31:0] instruction;
+
+if_stage DUT(
+    .clk(clk),
+    .reset(reset),
+    .stall(stall),
+    .pc_out(pc_out),
+    .instruction(instruction)
+);
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin
+
+    reset = 1;
+    stall = 0;
+
+    #10;
+    reset = 0;
+
+    #100;
+
+    $finish;
+end
+
+initial begin
+    $monitor(
+        "Time=%0t PC=%d Instruction=%h",
+        $time,
+        pc_out,
+        instruction
+    );
+end
+
+endmodule
